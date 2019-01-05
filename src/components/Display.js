1152d6c45d390648f9b627e05fd5449fc30cd604
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-05 14:53:17 +0800

    Auto scale the display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ displayValue }) => <div id="display">{displayValue}</div>;
+class Display extends Component {
+  state = {
+    scale: 1
+  };
+  displayRef = React.createRef();
+
+  componentDidUpdate() {
+    const { scale } = this.state;
+
+    const node = this.displayRef.current;
+    const parentNode = node.parentNode;
+
+    const parentWidth = parentNode.offsetWidth - 45;
+    const displayWidth = node.offsetWidth;
+    const actualScale = parentWidth / displayWidth;
+
+    if (scale === actualScale) {
+      return;
+    }
+
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale });
+    } else if (scale < 1) {
+      this.setState({ scale: 1 });
+    }
+  }
+
+  render() {
+    const { scale } = this.state;
+    const { displayValue } = this.props;
+
+    return (
+      <div
+        id="display"
+        style={{ transform: `scale(${scale})` }}
+        ref={this.displayRef}
+      >
+        {displayValue}
+      </div>
+    );
+  }
+}
 
 Display.propTypes = {
   displayValue: PropTypes.string.isRequired
--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const History = ({ history }) => <div>{history}</div>;
+const History = ({ history }) => <div className="history">{history}</div>;
 
 History.propTypes = {
   history: PropTypes.string.isRequired
