22fed98cdf89e3716732cba810fbe3a2a317bfa4
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-05 15:32:04 +0800

    Updates

---
+++ precache-manifest.039e8b3f81a88c4966e65b723a06c56b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "fa1ad03767d3b9efd017",
+    "url": "/javascript-calculator/static/js/main.fa1ad037.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/javascript-calculator/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "fa1ad03767d3b9efd017",
+    "url": "/javascript-calculator/static/css/main.4ab2827a.chunk.css"
+  },
+  {
+    "revision": "c6c8d5e92d58aa3aff9ea9cb3d06856c",
+    "url": "/javascript-calculator/index.html"
+  }
+];
